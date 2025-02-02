2ad3f3369c5d5896faf2c4ad6fb70789fce1206c
krnklvx <sawosit@gmail.com>
2025-02-02 17:11:45 +0300

    //

--- src/main/java/Elevator.java
+++ src/main/java/Elevator.java
@@ -1,7 +1,7 @@
 abstract class Elevator {
     protected ElevatorState state;
-    protected int currentFloor = 1; // Начинаем с 1-го этажа
-    protected Integer destinationFloor = null; // Этаж назначения
+    protected int currentFloor = 1;
+    protected Integer destinationFloor = null; 
 
     public abstract void call(int floor);
 
